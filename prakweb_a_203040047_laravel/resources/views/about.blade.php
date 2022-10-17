e9c0fa70bee6e2dcbb6383418075440970113af5
LeniSetiarini09 <leni.setiarini0903@gmail.com>
2022-10-17 19:40:36 +0700

    menyelesaikan video 4

--- prakweb_a_203040047_laravel/resources/views/about.blade.php
+++ prakweb_a_203040047_laravel/resources/views/about.blade.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>WPU BLOG | About</title>
-</head>
 
-<body>
-  <h1>Halaman About</h1>
-  <h3><?php echo $name; ?></h3>
-  <p><?php echo $email; ?></p>
-  <img src="img/<?php echo $image; ?>" alt="<?php echo $name; ?>" width="200">
-</body>
+@section('container')
 
-</html>
+
+<h1>Halaman About</h1>
+<h3>{{ $name }}</h3>
+<p>{{ $email }}</p>
+
+<img src="img/{{ $image; }}" alt="{{ $name; }}" width="200">
+@endsection
