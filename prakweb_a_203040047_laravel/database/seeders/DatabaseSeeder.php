55c39c619f1bac218351ee00c612ca8c7741eefe
LeniSetiarini09 <leni.setiarini0903@gmail.com>
2022-10-27 21:04:35 +0700

    menyelesaikan video 8

--- prakweb_a_203040047_laravel/database/seeders/DatabaseSeeder.php
+++ prakweb_a_203040047_laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,51 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Leni Setiarini',
+            'email' => 'lennulen@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'category_id' => 1,
+            'user_id' => 1
+
+        ]);
+
+        Category::create([
+            'name' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'category_id' => 1,
+            'user_id' => 1
+
+        ]);
+
+        Category::create([
+            'name' => 'Judul Ketiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'category_id' => 2,
+            'user_id' => 1
+
+        ]);
     }
 }
--- prakweb_a_203040047_laravel/resources/views/posts.blade.php
+++ prakweb_a_203040047_laravel/resources/views/posts.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
+<h1 class="mb-5">Halaman Blog Posts</h1>
 
 @foreach ($posts as $post)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-3">
   <h2>
-    <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
   </h2>
-  <h5>by: {{ $post["author"] }}</h5>
+
+  <p>By. Leni Setiarini in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+    {{ $post->category->name }}</a></p>
+
   <p>{{ $post->excerpt }}</p>
+
+  <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More..</a>
 </article>
 @endforeach
 
