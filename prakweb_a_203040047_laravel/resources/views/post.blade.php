956b3432decc3ac8747048f508d902d8e778d638
LeniSetiarini09 <leni.setiarini0903@gmail.com>
2022-10-31 21:57:53 +0700

    menyelesaikan video 12

--- prakweb_a_203040047_laravel/resources/views/post.blade.php
+++ prakweb_a_203040047_laravel/resources/views/post.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.main')
 
 @section('container')
-<article>
-  <h2>{{ $post->title }}</h2>
 
-  <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
-    {{ $post->category->name }}</a></p>
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+        <h2 class="mb-3 mt-3">{{ $post->title }}</h2>
 
-  <!-- <h5>{{ $post["author"] }}</h5> -->
-  {!! $post->body !!}
+        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none">{{$post->category->name}}</a></p>
 
-</article>
-<a href="/blog" class="d-block mt-3">back to posts</a>
+        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+        <article class="my-3 fs-5">
+        {!! $post->body !!}
+        </article>
+
+
+        <a href="/blog" class="d-block mt-3 text-decoration-none">Back to Post</a>
+
+        </div>
+    </div>
+</div>
+
 @endsection
